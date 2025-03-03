d0478816c65b69975cf410c72fecac28dcf41376
Kirill <kirill.rasshivalov@gmail.com>
2025-03-03 17:17:41 +0300

    Добавил контроллер который выводит все доступные конкретному пользователю отслеживаемые команды.

---
+++ scrapper/src/main/java/backend/academy/scrapper/controllers/ShowLinkController.java
@@ -0,0 +1,45 @@
+package backend.academy.scrapper.controllers;
+
+import backend.academy.dto.LinkInfoDTO;
+import backend.academy.dto.ShowListResponseDTO;
+import backend.academy.scrapper.managers.Collection;
+import backend.academy.scrapper.managers.ErrorHandler;
+import backend.academy.scrapper.data.LinkData;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+import static backend.academy.scrapper.components.LogComponent.loggFactory;
+
+/**
+ * Контроллер для вывода всех отслеживаемых ссылок данным пользователем.
+ */
+@RestController
+public class ShowLinkController {
+
+    @GetMapping("/links")
+    public ResponseEntity<?> showLink(@RequestHeader("tg-chat-id") String chatId) {
+        loggFactory.addServerLog("Пришел запрос на показ действующий ссылок " + chatId);
+        Long id = Long.parseLong(chatId);
+        if (Collection.idInfo.containsKey(id)) {
+            ShowListResponseDTO showListResponseDTO = new ShowListResponseDTO();
+            List<LinkData> linkInfoDTOS = Collection.idInfo.get(id);
+            List<LinkInfoDTO> linkInfoDTOList = new ArrayList<>();
+            for (LinkData linkData : linkInfoDTOS) {
+                linkInfoDTOList.add(new LinkInfoDTO(
+                    id,
+                    linkData.link(),
+                    linkData.tags(),
+                    linkData.filter())
+                );
+            }
+            showListResponseDTO.setLinks(linkInfoDTOList);
+            showListResponseDTO.setSize((long) linkInfoDTOList.size());
+            return ResponseEntity.ok().body(showListResponseDTO);
+        } else {
+            return ResponseEntity.badRequest().body(ErrorHandler.userHasNoLinks());
+        }
+    }
+}
