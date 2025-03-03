fd9b8cee67f0c5a51c32f0ba683d910234c649e7
Kirill <kirill.rasshivalov@gmail.com>
2025-03-03 17:16:25 +0300

    Добавил контроллер который осуществляет добавление пользователей в активные, если они еще там не были. Своего рода регистрация.

---
+++ scrapper/src/main/java/backend/academy/scrapper/controllers/RegisterUserController.java
@@ -0,0 +1,29 @@
+package backend.academy.scrapper.controllers;
+
+import backend.academy.scrapper.managers.Collection;
+import backend.academy.scrapper.managers.ErrorHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import static backend.academy.scrapper.components.LogComponent.loggFactory;
+
+/**
+ * Контроллер который регистрирует новых пользователей.
+ */
+@RestController
+@RequestMapping("/tg-chat")
+public class RegisterUserController {
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> addActiveUser(@PathVariable String id) {
+        loggFactory.addServerLog("Пришел запрос на регистрацию пользователя " + id);
+        if (Collection.activeUsers.contains(Long.valueOf(id))) {
+            return ResponseEntity.badRequest().body(ErrorHandler.chatHasAlreadyExist());
+        } else {
+            Collection.activeUsers.add(Long.valueOf(id));
+            return ResponseEntity.ok().build();
+        }
+    }
+}
