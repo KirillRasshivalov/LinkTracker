ebe93e3f8396ee5ab09a3f357e08b00681fc10dd
Kirill <kirill.rasshivalov@gmail.com>
2025-03-03 17:12:24 +0300

    Добавил контроллер для добавления новых отслеживаемых ссылок.

--- scrapper/src/main/java/backend/academy/scrapper/controllers/AddLinkController.java
+++ scrapper/src/main/java/backend/academy/scrapper/controllers/AddLinkController.java
@@ -1,28 +1,49 @@
 package backend.academy.scrapper.controllers;
 
 import backend.academy.dto.AddLinkRequestDTO;
-import backend.academy.scrapper.ErrorHandler;
+import backend.academy.dto.AddLinkResponseDTO;
+import backend.academy.scrapper.managers.Collection;
+import backend.academy.scrapper.managers.ErrorHandler;
+import backend.academy.scrapper.data.LinkData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
+import static backend.academy.scrapper.components.LogComponent.loggFactory;
 
+/**
+ * Контроллер на добавление отслеживаемых ссылок.
+ */
 @RestController
 public class AddLinkController {
-
-    private List<String> links = new ArrayList<>();
-
 
     @PostMapping("/links")
     public ResponseEntity<?> updateCollection(
         @RequestBody AddLinkRequestDTO requestDTO,
         @RequestHeader("tg-chat-id") String chatId
     ) {
-        if (requestDTO.getLink() != null) {
-            links.add(requestDTO.getLink());
-            AddLinkRequestDTO responseDTO = new AddLinkRequestDTO();
-            responseDTO.setLink(requestDTO.getLink());
+        loggFactory.addServerLog("Пришел запрос на добавление ссылки от " + chatId);
+        LinkData linkData = new LinkData(requestDTO.link, requestDTO.filters, requestDTO.tags);
+        Long id = Long.valueOf(chatId);
+        Collection.activeUsers.add(id);
+        if (!Collection.idInfo.containsKey(id) || !Collection.idInfo.get(id).contains(linkData)) {
+            if (Collection.idInfo.containsKey(id)) {
+                Collection.idInfo.get(Long.valueOf(chatId)).add(linkData);
+            } else {
+                Collection.idInfo.put(Long.valueOf(chatId), new ArrayList<>());
+                Collection.idInfo.get(Long.valueOf(chatId)).add(linkData);
+            }
+            if (Collection.linksOwners.containsKey(requestDTO.link)) {
+                Collection.linksOwners.get(requestDTO.link).add(id);
+            } else {
+                Collection.linksOwners.put(requestDTO.link, new ArrayList<>());
+                Collection.linksOwners.get(requestDTO.link).add(id);
+            }
+            AddLinkResponseDTO responseDTO = new AddLinkResponseDTO();
+            responseDTO.setId(chatId);
+            responseDTO.setUrl(requestDTO.getLink());
+            responseDTO.setFilters(requestDTO.getFilters());
+            responseDTO.setTags(requestDTO.getTags());
             return ResponseEntity.ok(responseDTO);
         } else {
             return ResponseEntity.badRequest().body(ErrorHandler.sameLinkError());
