56bb774774538dba84b37e8151f5e7760689e5fe
Kirill <kirill.rasshivalov@gmail.com>
2025-03-03 17:02:29 +0300

    Добавил отдельный лог для отслеживание логов с бота и скрапера по отдельности.

--- common/src/main/java/backend/academy/loggs/LoggFactory.java
+++ common/src/main/java/backend/academy/loggs/LoggFactory.java
@@ -12,15 +12,23 @@ import java.util.logging.SimpleFormatter;
 @Component
 public class LoggFactory {
 
-    private static Logger logger = Logger.getLogger(LoggFactory.class.getName());
+    private static Logger botLogger = Logger.getLogger(LoggFactory.class.getName());
+    private static Logger serverLogger = Logger.getLogger(LoggFactory.class.getName());
 
     public LoggFactory() throws IOException {
-        FileHandler fileHandler = new FileHandler("Logs.log", true);
-        logger.addHandler(fileHandler);
-        fileHandler.setFormatter(new SimpleFormatter());
+        FileHandler botFfileHandler = new FileHandler("BotLogs.log", true);
+        FileHandler serverFileHandler = new FileHandler("ServerLogs.log", true);
+        botLogger.addHandler(botFfileHandler);
+        botFfileHandler.setFormatter(new SimpleFormatter());
+        serverLogger.addHandler(serverFileHandler);
+        serverFileHandler.setFormatter(new SimpleFormatter());
     }
 
-    public static void addLog(String message) {
-        logger.info(message);
+    public static void addBotLog(String message) {
+        botLogger.info(message);
+    }
+
+    public static void addServerLog(String message) {
+        serverLogger.info(message);
     }
 }
