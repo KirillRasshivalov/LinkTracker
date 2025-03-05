0fd4a946ed42ca56d71105b850b6f7df22a64244
Kirill <kirill.rasshivalov@gmail.com>
2025-03-05 17:33:26 +0300

    Добавил класс для парсинга ссылки на стековерфлоу.

---
+++ scrapper/src/main/java/backend/academy/scrapper/managers/StackOverflowLinkParser.java
@@ -0,0 +1,14 @@
+package backend.academy.scrapper.managers;
+
+/**
+ * Класс для парсинга ссылок на стековерфлоу.
+ */
+public class StackOverflowLinkParser {
+    public static String parseQuestionId(String url) {
+        String[] parts = url.split("/");
+        if (parts.length < 5 || !parts[3].equals("questions")) {
+            throw new IllegalArgumentException(url);
+        }
+        return parts[4];
+    }
+}
