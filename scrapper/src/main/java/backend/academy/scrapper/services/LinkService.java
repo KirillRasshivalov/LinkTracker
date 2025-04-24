26a5d15a6673f946327d02d58f07f53258409fa9
Kirill <kirill.rasshivalov@gmail.com>
2025-04-24 20:53:32 +0300

    Сделал класс сервис для управления табицей с ссылками.

---
+++ scrapper/src/main/java/backend/academy/scrapper/services/LinkService.java
@@ -0,0 +1,23 @@
+package backend.academy.scrapper.services;
+
+import backend.academy.scrapper.jpaRepositories.LinkInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Сервис для управления таблицы с ссылками.
+ */
+@Service
+@RequiredArgsConstructor
+public class LinkService {
+
+    private final LinkInfoRepository linkInfoRepository;
+
+    public boolean findLink(String link, Long userId) {
+        return linkInfoRepository.existsLinkForUser(userId, link);
+    }
+
+    public boolean findConnectedLinks(Long userId) {
+        return linkInfoRepository.userHasAnyLinks(userId);
+    }
+}
