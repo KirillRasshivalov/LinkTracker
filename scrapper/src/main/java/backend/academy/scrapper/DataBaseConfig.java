cb114233f1d1c4c9392c6f3f5fdb80b6fd084951
Kirill <kirill.rasshivalov@gmail.com>
2025-04-24 20:56:09 +0300

    Добавил конфиг для базы данных, чтобы брать информацию о подключение к бд из application класса.

---
+++ scrapper/src/main/java/backend/academy/scrapper/DataBaseConfig.java
@@ -0,0 +1,9 @@
+package backend.academy.scrapper;
+
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "spring.datasource")
+public record DataBaseConfig(@NotEmpty String url, @NotEmpty String username, @NotEmpty String password) {}
