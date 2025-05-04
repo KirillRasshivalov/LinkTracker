d908014aa1a399f53e48d7e504dd84ea9cb487be
Kirill <kirill.rasshivalov@gmail.com>
2025-05-04 20:58:05 +0300

    Добавил интерфейс и класс с реализацией общения скрапера и бота.

--- scrapper/src/main/java/backend/academy/scrapper/notifications/HTTPSender.java
+++ scrapper/src/main/java/backend/academy/scrapper/notifications/HTTPSender.java
@@ -2,6 +2,8 @@ package backend.academy.scrapper.notifications;
 
 import backend.academy.dto.LinkUpdateRequestDTO;
 import backend.academy.dto.MainInfoFromGithubDTO;
+import backend.academy.dto.MainInfoFromStackOverlowDTO;
+import backend.academy.scrapper.managers.CheckLink;
 import backend.academy.scrapper.services.ServerLogger;
 import java.util.List;
 import org.springframework.http.HttpEntity;
@@ -11,29 +13,56 @@ import org.springframework.web.client.RestTemplate;
 
 /** Реализация метода отправки ответа по средствам http протокола. */
 public class HTTPSender implements Sender {
+    private final CheckLink checkLink = new CheckLink();
 
     @Override
-    public void sendNotification(String link, List<Long> IDS, MainInfoFromGithubDTO info) {
+    public void sendNotification(String link, List<Long> IDS, Object info) {
+        if (checkLink.isGitHubLink(link)) {
+            MainInfoFromGithubDTO mainInfoFromGithubDTO = (MainInfoFromGithubDTO) info;
+            for (int i = 0; i < IDS.size(); i++) {
+                LinkUpdateRequestDTO linkUpdateRequestDTO = new LinkUpdateRequestDTO();
+                RestTemplate restTemplate = new RestTemplate();
+                linkUpdateRequestDTO.setUrl(link);
+                linkUpdateRequestDTO.setId(IDS.get(i));
+                linkUpdateRequestDTO.setTgChatIds(IDS);
+                linkUpdateRequestDTO.setDescription("Пришло обновление по ссылке: " + link + ".\n" + "Автор: "
+                        + mainInfoFromGithubDTO.authorName() + ".\n" + "Содержание: "
+                        + mainInfoFromGithubDTO.message() + ".\n" + "Название " + mainInfoFromGithubDTO.type() + ": "
+                        + mainInfoFromGithubDTO.nameOfAnswer() + ".\n" + "Время последнего коммита: "
+                        + mainInfoFromGithubDTO.createdAt() + ".\n");
+                String botUrl = "http://localhost:8080/updates";
+                HttpEntity<LinkUpdateRequestDTO> httpEntity = new HttpEntity<>(linkUpdateRequestDTO);
 
-        for (int i = 0; i < IDS.size(); i++) {
-            LinkUpdateRequestDTO linkUpdateRequestDTO = new LinkUpdateRequestDTO();
-            RestTemplate restTemplate = new RestTemplate();
-            linkUpdateRequestDTO.setUrl(link);
-            linkUpdateRequestDTO.setId(IDS.get(i));
-            linkUpdateRequestDTO.setTgChatIds(IDS);
-            linkUpdateRequestDTO.setDescription("Пришло обновление по ссылке: " + link + ".\n" + "Автор: "
-                    + info.authorName() + ".\n" + "Содержание: "
-                    + info.message() + ".\n" + "Название issue: "
-                    + info.nameOfIssue() + ".\n" + "Время последнего коммита: "
-                    + info.createdAt() + ".\n");
-            String botUrl = "http://localhost:8080/updates";
-            HttpEntity<LinkUpdateRequestDTO> httpEntity = new HttpEntity<>(linkUpdateRequestDTO);
+                ResponseEntity<String> response =
+                        restTemplate.exchange(botUrl, HttpMethod.POST, httpEntity, String.class);
+                ServerLogger.LOGGER
+                        .atInfo()
+                        .setMessage("Получен ответ:" + response.getBody())
+                        .log();
+            }
+        } else {
+            MainInfoFromStackOverlowDTO mainInfoFromStackOverlowDTO = (MainInfoFromStackOverlowDTO) info;
+            for (int i = 0; i < IDS.size(); i++) {
+                LinkUpdateRequestDTO linkUpdateRequestDTO = new LinkUpdateRequestDTO();
+                RestTemplate restTemplate = new RestTemplate();
+                linkUpdateRequestDTO.setUrl(link);
+                linkUpdateRequestDTO.setId(IDS.get(i));
+                linkUpdateRequestDTO.setTgChatIds(IDS);
+                linkUpdateRequestDTO.setDescription("Пришло обновление по ссылке: " + link + ".\n" + "Автор: "
+                        + mainInfoFromStackOverlowDTO.name() + ".\n" + "Тема: "
+                        + mainInfoFromStackOverlowDTO.theme() + ".\n" + "Комментарий: "
+                        + mainInfoFromStackOverlowDTO.answer() + ".\n" + "Время последнего коммита: "
+                        + mainInfoFromStackOverlowDTO.time() + ".\n");
+                String botUrl = "http://localhost:8080/updates";
+                HttpEntity<LinkUpdateRequestDTO> httpEntity = new HttpEntity<>(linkUpdateRequestDTO);
 
-            ResponseEntity<String> response = restTemplate.exchange(botUrl, HttpMethod.POST, httpEntity, String.class);
-            ServerLogger.LOGGER
-                    .atInfo()
-                    .setMessage("Получен ответ:" + response.getBody())
-                    .log();
+                ResponseEntity<String> response =
+                        restTemplate.exchange(botUrl, HttpMethod.POST, httpEntity, String.class);
+                ServerLogger.LOGGER
+                        .atInfo()
+                        .setMessage("Получен ответ:" + response.getBody())
+                        .log();
+            }
         }
     }
 }
--- scrapper/src/main/java/backend/academy/scrapper/notifications/Sender.java
+++ scrapper/src/main/java/backend/academy/scrapper/notifications/Sender.java
@@ -1,9 +1,8 @@
 package backend.academy.scrapper.notifications;
 
-import backend.academy.dto.MainInfoFromGithubDTO;
 import java.util.List;
 
 /** Интерфейс для общения скрапера и бота. */
 public interface Sender {
-    void sendNotification(String link, List<Long> IDS, MainInfoFromGithubDTO info);
+    void sendNotification(String link, List<Long> IDS, Object info);
 }
