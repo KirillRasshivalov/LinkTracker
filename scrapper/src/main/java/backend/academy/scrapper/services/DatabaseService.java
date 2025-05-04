519422d49d6d23676a9d7238dce23968734b2960
Kirill <kirill.rasshivalov@gmail.com>
2025-05-04 21:03:17 +0300

    Обновил сервис для работы с репозиториями базы данных.

--- scrapper/src/main/java/backend/academy/scrapper/services/DatabaseService.java
+++ scrapper/src/main/java/backend/academy/scrapper/services/DatabaseService.java
@@ -1,10 +1,8 @@
 package backend.academy.scrapper.services;
 
 import backend.academy.dto.LinkInfoDTO;
-import backend.academy.scrapper.jpaRepositories.LinkInfoRepository;
-import backend.academy.scrapper.jpaRepositories.UsersRepository;
+import backend.academy.scrapper.managers.Datasourse;
 import backend.academy.scrapper.models.LinkInfo;
-import backend.academy.scrapper.models.Users;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -12,51 +10,38 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+/**
+ * Сервис для работы с репозиториями базы данных.
+ */
 @Service
 @RequiredArgsConstructor
 public class DatabaseService {
-    private final UsersRepository usersRepo;
-    private final LinkInfoRepository linksRepo;
+    private final Datasourse database;
 
     public void addUser(Long id) {
-        Users user = new Users();
-        user.userId(id);
-        usersRepo.save(user);
+        database.addUser(id);
     }
 
     public void deleteUser(Long id) {
-        usersRepo.deleteByUserId(id);
-        usersRepo.findByUserId(id).ifPresent(usersRepo::delete);
+        database.deleteUser(id);
     }
 
     @Transactional
     public void addLink(Long userId, String link, String filter, String teg) {
-        Users user = usersRepo
-                .findByUserId(userId)
-                .orElseThrow(() -> new IllegalArgumentException("User not found with id: " + userId));
-
-        LinkInfo linkInfo = new LinkInfo();
-        linkInfo.link(link);
-        linkInfo.filters(filter);
-        linkInfo.tegs(teg);
-        linksRepo.save(linkInfo);
-
-        user.links().add(linkInfo);
-        linkInfo.users().add(user);
-        usersRepo.save(user);
+        database.addLink(userId, link, filter, teg);
     }
 
     @Transactional
     public void deleteLink(Long userId, String link) {
-        linksRepo.deleteByUserAndLink(userId, link);
+        database.deleteLink(userId, link);
     }
 
     public List<LinkInfoDTO> showLinks(Long userId) {
-        return linksRepo.findLinksByUser(userId);
+        return database.showLinks(userId);
     }
 
     @Transactional
     public Page<LinkInfo> showLinks(Pageable pageable) {
-        return linksRepo.findAllLinks(pageable);
+        return database.showLinks(pageable);
     }
 }
