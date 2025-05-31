e7e9731fd9d06fdf1dcead12b6b78cb9a1874052
Kirill <kirill.rasshivalov@gmail.com>
2025-05-31 22:50:18 +0300

    Написал шаблон продюсера для отправки сообщений в очереди.:

---
+++ scrapper/src/main/java/backend/academy/scrapper/kafka/Producer.java
@@ -0,0 +1,27 @@
+package backend.academy.scrapper.kafka;
+
+import backend.academy.dto.LinkUpdateRequestDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * Продюсер для отправки сообщений в очередь.
+ */
+@Service
+@RequiredArgsConstructor
+public class Producer {
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+        .registerModule(new JavaTimeModule());
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(LinkUpdateRequestDTO message, boolean isError) throws JsonProcessingException {
+        String topic = isError ? "error-messages" : "valid-messages";
+        String jsonMessage = objectMapper.writeValueAsString(message);
+        kafkaTemplate.send(topic, String.valueOf(message.getId()), jsonMessage);
+    }
+}
