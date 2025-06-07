7b429f2008fcdf7c365f4daf5bbb3b379b0807b6
Kirill <kirill.rasshivalov@gmail.com>
2025-06-07 17:17:06 +0300

    Добавил аннотацию позволяющую кэшировать данные.:

--- bot/src/main/java/backend/academy/bot/BotApplication.java
+++ bot/src/main/java/backend/academy/bot/BotApplication.java
@@ -3,8 +3,10 @@ package backend.academy.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @EnableConfigurationProperties({BotConfig.class})
 public class BotApplication {
     public static void main(String[] args) {
